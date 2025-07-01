234bac6452c1aa990d32ce1c5f0d0be69fc9ce29
anant16-devops <anant.deora@accenture.com>
2025-07-01 12:46:20 +0000

    learn: terraform beginner code

---
+++ Beginner/AWS/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">=1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "anant-devops-iaac-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
