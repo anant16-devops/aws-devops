8ba3e1de3d6d64d18813430ea9c2ee6ec05b7c5c
Deora <anant.deora@accenture.com>
2025-07-02 17:25:51 +0530

    Basic Lambda Execution Permission attached to lambda iam role

--- Beginner/AWS/main.tf
+++ Beginner/AWS/main.tf
@@ -59,6 +59,11 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  }
+
+resource "aws_iam_role_policy_attachment" "lambda_permission_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.iam_for_lambda.name
 }
 
 resource "aws_lambda_permission" "allow_bucket" {
